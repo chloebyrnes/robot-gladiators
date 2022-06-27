0cded029b29c7c33b4127100eea32a2b3e53d1e5
chloebyrnes <107075274+chloebyrnes@users.noreply.github.com>
2022-06-27 13:49:12 -0400

    added fight

--- assests/game.java
+++ assests/game.java
@@ -1,22 +1,39 @@
-// notes for java
-
+// Data on robot
 var playerName = window.prompt("What is your robot's name?");
-// What is this?
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log("This logs a string, good for leaving yourself a message");
-// this will do math and log 20
-console.log(10 + 10);
-// what is this?
-console.log("Our robot's name is " + playerName);
+// Logging this info in console
+console.log(playerName, playerAttack, playerHealth);
 
-// Note the lack of quotation marks around playerName
-window.alert(playerName);
+// Data on enemy
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-function fight() {
-    window.alert("The fight has begun!");
-  }
+// Fight data
+// create function
+var fight = function() {
+  // Alert players that they are starting the round
+  window.alert("Welcome to Robot Gladiators!");
 
-// fight();
+//Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+enemyHealth = enemyHealth - playerAttack;
 
-  fight();
+// Log a resulting message to the console so we know that it worked.
+console.log(
+  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+);
+
+// Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable
+playerHealth = playerHealth - enemyAttack;
+
+// Log a resulting message to the console so we know that it worked.
+console.log(
+  enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+);
+
+};
+
+// execute function
+fight();
